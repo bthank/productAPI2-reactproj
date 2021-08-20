3a141d16eef339078ea57deccd593f4a3f9e2f34
bthank <bthankachan@yahoo.com>
2021-08-20 11:34:50 -0400

    Read operation changes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var bodyparser = require("body-parser");
+var cors = require("cors");
 
 var app = express();
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:false}));
+app.use(cors());
 
 var productAPI2 = require("./controllers/product.controller");
 
